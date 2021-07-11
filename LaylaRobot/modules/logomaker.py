d7d10728ec8c272660130e30ff49dcfe7cece445
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-07-11 16:06:18 +0530

    Update logomaker.py

--- LaylaRobot/modules/logomaker.py
+++ LaylaRobot/modules/logomaker.py
@@ -100,7 +100,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByLayla.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @mininbotz Support @kidbots")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @LyciRobot Support @kidbots")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -156,4 +156,4 @@ __help__ = """
  ❍ /wlogo text :  Create your logo with your name
 
  """
-__mod_name__ = "Logo"
+__mod_name__ = "ʟᴏɢᴏ"
