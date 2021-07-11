1c136eb7890ce132415e29b52fc6a8596219de88
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-07-11 15:44:57 +0530

    Update blacklist_stickers.py

--- LaylaRobot/modules/blacklist_stickers.py
+++ LaylaRobot/modules/blacklist_stickers.py
@@ -494,7 +494,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "Stickers b list"
+__mod_name__ = "ʙ-ꜱᴛɪᴄᴋ"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
