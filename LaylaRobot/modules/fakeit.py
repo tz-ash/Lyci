799674a039d4a1337e97be5e5b79793005a4a02e
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-07-14 10:24:25 +0530

    Update fakeit.py

--- LaylaRobot/modules/fakeit.py
+++ LaylaRobot/modules/fakeit.py
@@ -44,7 +44,7 @@ async def _(event):
             with open("FRIDAYOT.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by @AwesomeSupport."
+        captin = f"Fake Image powered by @tzkid."
         fole = "FRIDAYOT.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
